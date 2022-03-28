2c841283dcf7e024a39e4713d16f37ef34b9f861
Doiae9 <leonardogh75@gmail.com>
2022-03-28 11:54:46 -0600

    Función

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int contA=0,contB=0;
+        for (int i = 0; i < a.size(); i++) {
+            if(a.get(i))>b.get(i){
+     contA++;
+            }else if (b.get(i>a.get(i))){
+                contB++;
+            }else{
+
+            }
+            ArrayList<Integer> lista=new ArrayList<>(Arrays.asList(contA,contB));
+            return lista;
+        }
+    }
+}
